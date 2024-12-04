efe05d0a97d225c5680b46b226c0b1b3587e3837
Jhanoi Samuels <jhanoi.christina@gmail.com>
2024-12-03 19:22:46 -0500

    Edited schema file

--- schema.sql
+++ schema.sql
@@ -2,41 +2,43 @@ CREATE DATABASE dolphin_crm;
 USE dolphin_crm;
 
 --Creating the Users Table
-CREATE TABLE Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(35) UNIQUE NOT NULL,
-    lastname VARCHAR(35) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    role VARCHAR(60),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-);
+CREATE TABLE `dolphin_crm`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT, 
+    `firstname` VARCHAR(35) NOT NULL, 
+    `lastname` VARCHAR(35) NOT NULL, 
+    `password` VARCHAR(255) NOT NULL, 
+    `email` VARCHAR(100) NOT NULL, 
+    `role` VARCHAR(60) NOT NULL, 
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 --Creating the Contacts Table
-CREATE TABLE Contacts(
-    contact_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(20), 
-    firstname VARCHAR(35) UNIQUE NOT NULL,
-    lastname VARCHAR(35) UNIQUE NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    telephone VARCHAR(15),
-    company VARCHAR(100),
-    type_role VARCHAR(11) NOT NULL,
-    assigned_to INT,
-    created_by INT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (assigned_to) REFERENCES Users(user_id),
-    FOREIGN KEY (created_by) REFERENCES Users(user_id)
-);
+CREATE TABLE `dolphin_crm`.`contact` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    `title` VARCHAR(20) NOT NULL,
+    `firstname` VARCHAR(35) NOT NULL,
+    `lastname` VARCHAR(35) NOT NULL ,
+    `email` VARCHAR(100) NOT NULL,
+    `telephone` VARCHAR(15) NOT NULL,
+    `company` VARCHAR(100) NOT NULL,
+    `type` VARCHAR(11) NOT NULL,
+    `assigned_to` INT NOT NULL, 
+    `created_by` INT NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`assigned_to`) REFERENCES `users`(`id`),
+    FOREIGN KEY (`created_by`) REFERENCES `users`(`id`)) ENGINE = InnoDB;
 
 --Creating the Notes Table
-CREATE TABLE Notes(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    contact_id INT,
-    comment TEXT,
-    created_by INT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (contact_id) REFERENCES Contacts(contact_id),
-    FOREIGN KEY (created_by) REFERENCES Users(user_id)
-);
+CREATE TABLE `dolphin_crm`.`notes` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `contact_id` INT NOT NULL,
+    `comment` TEXT NOT NULL,
+    `created_by` INT NOT NULL ,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`contact_id`) REFERENCES `contact`(`contact_id`),
+    FOREIGN KEY (`created_by`) REFERENCES `users`(`id`)) ENGINE = InnoDB;
+
+--Dumping data for the users table
+INSERT INTO `users` (`password`, `email`) 
+VALUES ('$2y$10$ddIpI4n.X.BnDJJeK1bhY.heVPZZx7TG.sPkCrdjdyGXG2bu7snb6', 'admin@project2.com');
